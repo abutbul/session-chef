7ab84e52e41bad111964b2b6e326b8bf4e655215
david abutbul <david@abutbul.com>
2016-10-28 00:24:53 +0300

    now use dedicated folder

--- HomeWork0/cookbooks/my-first-cookbook/recipes/mkfile.rb
+++ HomeWork0/cookbooks/my-first-cookbook/recipes/mkfile.rb
@@ -1,13 +1,20 @@
 #
 # Cookbook Name:: my-first-cookbook
-# Recipe:: default
+# Recipe:: mkfile
 #
 # Copyright 2016, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
 #
-cookbook_file '/tmp/david.txt' do
+
+directory '/opt/opsSchool' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+cookbook_file '/opt/opsSchool/david.txt' do
   source 'mkfile/just-text.txt'
   action :create
   owner 'root'
