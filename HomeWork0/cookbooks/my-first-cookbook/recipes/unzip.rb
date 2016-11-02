d4fc30db9f4e28a42a284a7682153f1d9c64c7a6
david abutbul <david@abutbul.com>
2016-11-02 05:01:17 +0200

    adding unzip with version lock

---
+++ HomeWork0/cookbooks/my-first-cookbook/recipes/unzip.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: my-first-cookbook
+# Recipe:: unzip
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+package "unzip" do
+  version '6.0-9ubuntu1.5'
+  action :install
+end
