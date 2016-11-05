4d72ce62613fc49dfd3bee02220530f4e564cbc9
david abutbul <david@abutbul.com>
2016-11-06 00:29:44 +0200

    assignment 2 part 1

--- HomeWork1/cookbooks/my-first-cookbook/recipes/default.rb
+++ HomeWork1/cookbooks/my-first-cookbook/recipes/default.rb
@@ -18,3 +18,39 @@ execute 'Install python package' do
 	not_if { File.exist?("/usr/local/lib/python2.7/dist-packages/flask") }
 end
 
+cookbook_file node['mycookbook']['app']['db_creation_script'] do
+  action :create
+  owner 'root'
+  group 'root'
+  notifies :run, 'execute[CREATE USER]', :delayed
+  notifies :run, 'execute[makeAARdb]', :immediate
+end
+
+execute 'makeAARdb' do
+  action :nothing
+  command "mysql -p#{node['mycookbook']['app']['mysql_pass']} < #{node['mycookbook']['app']['db_creation_script']}"
+  not_if "mysql -p#{node['mycookbook']['app']['mysql_pass']} -e 'show databases;'|grep #{node['mycookbook']['app']['db_name']}"
+end
+
+execute 'CREATE USER' do
+  action :nothing
+  command "mysql -p#{node['mycookbook']['app']['mysql_pass']} -e \"GRANT ALL PRIVILEGES ON #{node['mycookbook']['app']['db_name']}.* TO '#{node['mycookbook']['app']['db_user']}'@#{node['mycookbook']['app']['db_host']} IDENTIFIED BY '#{node['mycookbook']['app']['db_pass']}';\""
+  not_if "mysql -p#{node['mycookbook']['app']['mysql_pass']} -e 'select user from mysql.user;'|grep #{node['mycookbook']['app']['db_name']}"
+end
+
+template node['mycookbook']['apache']['conf_file'] do
+  source 'AAR-apache.conf.erb'
+  owner node['mycookbook']['apache']['user']
+  group node['mycookbook']['apache']['group'] 
+  action :create
+  notifies :restart, 'service[apache2]', :immediately
+end
+
+service 'apache2' do
+  action [:start, :enable]
+end
+template node['mycookbook']['app']['config_file'] do
+  source 'AAR_config.py.erb'
+  action :create
+end
+
--- HomeWork1/cookbooks/my-first-cookbook/spec/default_spec.rb
+++ HomeWork1/cookbooks/my-first-cookbook/spec/default_spec.rb
@@ -1,16 +1,43 @@
+#TODO how do I call attributes from here,,,
+#TODO how can i avoid repeating defining sqlpopulate
 require 'chefspec'
 
 describe 'my-first-cookbook::default' do
 
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe) }
-
-
+    
+    before do
+      stub_command("dpkg -l | grep python-pip | grep ii").and_return(0)
+      stub_command("dpkg -l | grep libapache2-mod-wsgi | grep ii").and_return(0)
+      stub_command("dpkg -l | grep python-mysqldb | grep ii").and_return(0)
+    end
+    
+    
     it 'creates apache config template with a VirtualHost definition' do
+      expect(chef_run).to create_template('/etc/apache2/sites-enabled/AAR-apache.conf')
+      expect(chef_run).to render_file('/etc/apache2/sites-enabled/AAR-apache.conf').with_content {|content|
+        expect(content).to include('<VirtualHost *:80>')
+        expect(content).to include('</VirtualHost>')
+      }
     end
 
     it 'restarts apache2 service on config file change' do
+      filechanged = chef_run.template('/etc/apache2/sites-enabled/AAR-apache.conf')
+      expect(filechanged).to notify('service[apache2]').to(:restart).immediately
     end
 
     it 'starts and enables apache2 service' do
+      expect(chef_run).to start_service('apache2')
+      expect(chef_run).to enable_service('apache2')
+    end
+
+    it 'populates DB on dump deployment' do
+      sqlpopulate = chef_run.cookbook_file('/tmp/make_AARdb.sql')
+      expect(sqlpopulate).to notify('execute[makeAARdb]').to(:run).immediately
+    end
+
+    it 'creates and grants permissions to app db user' do
+      sqlpopulate = chef_run.cookbook_file('/tmp/make_AARdb.sql')
+      expect(sqlpopulate).to notify('execute[CREATE USER]').to(:run).delayed
     end
 end
